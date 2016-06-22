c179a35cbeff7f7fd1778a5b618db122e9ad9aec
Vimo <929992114@qq.com>
2016-06-22 23:39:18 +0800

    re search vs match

--- py/re/re_test.py
+++ py/re/re_test.py
@@ -4,15 +4,28 @@ import re
 re 模块的测试
 """
 
-str1 = 'food'
-m1 = re.match('foo', str1)
-print(m1)
-print(m1.group())
-print(m1.groups())
 
-m = re.compile('foo')
-m2 = m.match(str1)
-print(m2)
-print(m2.group())
-print(m2.groups())
+def test_compile():
+    str1 = 'food'
+    m1 = re.match('foo', str1)
+    print(m1)
+    print(m1.group())
+    print(m1.groups())
 
+    m = re.compile('foo')
+    m2 = m.match(str1)
+    print(m2)
+    print(m2.group())
+    print(m2.groups())
+
+
+def test_search():
+    m = re.match('foo', 'seafood')
+    print(m)
+    m1 = re.search('foo', 'seafood food')
+    print(m1)
+    print(m1.group())
+    print(m1.groups())
+
+
+test_search()
