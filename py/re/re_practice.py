4e51434353bc8a9ffdf85c6fecd4ba6a2e63e51e
Vimo <929992114@qq.com>
2016-06-25 23:45:44 +0800

    re practice

--- py/re/re_practice.py
+++ py/re/re_practice.py
@@ -20,3 +20,17 @@ def re_practice1():
     """
     re.match(r'\w+[\s|,]\w+', 'I,m')
 
+
+def re_practice2():
+    """
+    匹配python标识符的集合
+    """
+    re.match('[a-zA-Z_$]\w*', 'python')
+    """
+    匹配美国街道名称
+    """
+    re.match('\d+(\s\w+)+', '10082 Da le me')
+    """
+    匹配www起始 .com结尾的域名
+    """
+    re.match(r'www://(\w+.)+?\w+', 'www://www.bing.com')
