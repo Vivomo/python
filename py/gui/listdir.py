46d93ea65c9afb0f0f221f8ad2b7c853cfec93b4
Vimo <929992114@qq.com>
2016-07-04 23:32:02 +0800

    listdir2

--- py/gui/listdir.py
+++ py/gui/listdir.py
@@ -19,18 +19,76 @@ class DirList(object):
         self.dirsb = Scrollbar(self.dirfm)
         self.dirsb.pack(side=RIGHT, fill=Y)
         self.dirs = Listbox(self.dirfm, height=15, width=50, yscrollcommand=self.dirsb.set)
-        self.dirs.bind('<Double-1>')
+        self.dirs.bind('<Double-1>', self.setDirAndGo)
+        self.dirsb.config(command=self.dirs.yview)
+        self.dirs.pack(side=LEFT, fill=BOTH)
+        self.dirfm.pack()
+
+        self.dirn = Entry(self.top, width=50, textvariable=self.cwd)
+        self.dirn.bind('<Return>', self.doLS)
+        self.dirn.pack()
+
+        self.bfm = Frame(self.top)
+        self.clr = Button(self.bfm, text='Clear', command=self.clrDir, activeforeground='white',
+                          activebackground='red')
+        self.ls = Button(self.bfm, text='List Directory', command=self.doLS, activeforeground='white',
+                         activebackground='green')
+        self.quit = Button(self.bfm, text='QUIT', command=self.top.quit, activeforeground='white',
+                           activebackground='red')
+        self.clr.pack(side=LEFT)
+        self.ls.pack(side=LEFT)
+        self.quit.pack(side=LEFT)
+        self.bfm.pack()
+
+        if initdir:
+            self.cwd.set(os.curdir)
+            self.doLS()
 
     def clrDir(self, ev=None):
         self.cwd.set('')
 
     def setDirAndGo(self, ev=None):
-        pass
+        self.last = self.cwd.get()
+        self.dirs.config(selectbackground='red')
+        check = self.dirs.get(self.dirs.curselection())
+        if not check:
+            check = os.curdir
+        self.cwd.set(check)
+        self.doLS()
 
     def doLS(self, ev=None):
-        pass
+        error = ''
+        tdir = self.cwd.get()
+        if not tdir:
+            tdir = os.curdir
+        if not os.path.exists(tdir):
+            error = tdir + ': no such file'
+        elif not os.path.isdir(tdir):
+            error = tdir + ': is not a directory'
 
-    
+        if error:
+            self.cwd.set(error)
+            self.top.update()
+            sleep(2)
+            if not (hasattr(self, 'last')) and self.last:
+                self.last = os.curdir
+            self.cwd.set('FETCHING DIRECTORY CONTENTS...')
+            self.top.update()
+            dirlist = os.listdir(tdir)
+            dirlist.sort()
+            os.chdir(tdir)
+
+            self.dirl.config(text=os.getcwd())
+            self.dirs.delete(0, END)
+            self.dirs.insert(END, os.curdir)
+            self.dirs.insert(END, os.pardir)
+            for eachFile in dirlist:
+                self.dirs.insert(END, eachFile)
+            self.cwd.set(os.curdir)
+            self.dirs.config(selectbackground='LightSkyBlue')
+
+
+
 def main():
     d = DirList(os.curdir)
     mainloop()
