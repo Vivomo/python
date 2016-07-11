c5f980834a761319c73d875e6ddb478e39c06369
vivomo <929992114@qq.com>
2016-07-11 19:58:07 +0800

    no message

--- py/translate/del_surplus_var2.py
+++ py/translate/del_surplus_var2.py
@@ -7,23 +7,30 @@ from py.translate import line as L
 
 enPath = r'E:\SHT\project\sas-web\src\main\webapp\WEB-INF\views\lang\en'
 zhPath = r'E:\SHT\project\sas-web\src\main\webapp\WEB-INF\views\lang\zh'
+testFile = r'E:\SHT\project\sas-web\src\main\webapp\WEB-INF\views\lang\zh\l_event.ftl'
 varReg = re.compile(r'(l_\w+)\s?=\s?".+?"')
+
+
+def del_var(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        varArr = []
+        delArr = []
+        lines = file.readlines()
+        lines.reverse()
+        length = len(lines)
+        for i, l in enumerate(lines):
+            result = re.search(varReg, l)
+            if result:
+                var = result.groups()[0]
+                if var in varArr:
+                    delArr.append(str(length - i))
+                else:
+                    varArr.append(var)
+        # IO_util.del_file_lines(file_path, delArr)
+        print(file_path, '-'.join(delArr))
+
 
 for path in [enPath, zhPath]:
     for f in IO_util.get_all_file(path):
-        with open(f, 'r', encoding='utf-8') as file:
-            varArr = []
-            delArr = []
-            lines = file.readlines()
-            lines.reverse()
-            length = len(lines)
-            for i, l in enumerate(lines):
-                result = re.search(varReg, l)
-                if result:
-                    var = result.groups()[0]
-                    if var in varArr:
-                        delArr.append(length - i)
-                    else:
-                        varArr.append(var)
-            IO_util.del_file_lines(f, delArr)
-            # print(f, '-'.join(delArr))
+        del_var(f)
+# del_var(testFile)
