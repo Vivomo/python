18817f3c8147eb2e43cf065f375dfdbd5c51ea41
vivomo <929992114@qq.com>
2016-07-18 15:41:31 +0800

    no message

--- py/translate/upper_first_letter.py
+++ py/translate/upper_first_letter.py
@@ -4,11 +4,12 @@ from py.utils import IO_util
 
 enPath = r'E:\SHT\project\sas-web\src\main\webapp\WEB-INF\views\lang\en'
 # zhPath = r'E:\SHT\project\sas-web\src\main\webapp\WEB-INF\views\lang\zh'
-lineReg = re.compile(r'l_\w+\s*=\s*".+?"')
+lineReg = re.compile(r'\w+\s*:\s*\'.+?\'')
 
 
 def capitalize_line(line):
-    index = line.find('"')
+    index = line.find('\'')
+    # index = line.find('"')
     return line[:index+1] + line[index+1].upper() + line[index+2:]
 
 
@@ -23,7 +24,7 @@ def capitalize_file(file_path):
                 line_arr.append(line)
     IO_util.write_to_file(file_path, ''.join(line_arr))
 
-for p1 in IO_util.get_all_file(enPath):
-    capitalize_file(p1)
-# capitalize_file(r'E:\git\pythonCode\test\translate\write\en\event_signup.ftl')
+# for p1 in IO_util.get_all_file(enPath):
+#     capitalize_file(p1)
+# capitalize_file(r'E:\SHT\project\sas-web\src\main\webapp\src\js\language\language.js')
 
