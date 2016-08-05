bf9d12476e8a6c7dc76c678699258623cf30d849
vivomo <929992114@qq.com>
2016-08-05 14:07:38 +0800

    no message

---
+++ py/draw/test.py
@@ -0,0 +1,14 @@
+from PIL import Image, ImageDraw
+from matplotlib import patches, pyplot
+#
+# img = Image.new('RGB', (100, 100))
+# draw = ImageDraw.Draw(img)
+# draw.arc(50, 50, 90, 180)
+# img.save('../../src/d1.png', 'png')
+
+# im = Image.new('RGBA', (200, 200))
+# draw = ImageDraw.Draw(im)
+# draw.arc((50, 50, 150, 150), 100, 300, fill="#00ffff")
+# im.save('1.png')
+
+arc = patches.Arc(100, 100, 100, 100, 0, 100)
