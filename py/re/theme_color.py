b5c8c0c40c9552efd66ceb3f19644e3bacedea1a
vivomo <929992114@qq.com>
2016-08-16 20:14:07 +0800

    no message

--- py/re/theme_color.py
+++ py/re/theme_color.py
@@ -1,18 +1,43 @@
 from py.utils import IO_util
 import re
 
-# 获取theme4
-filePath = r'E:\SHT\project\sas-web\src\main\webapp\src\default\scss'
 theme4Reg = re.compile(r'\$theme4:\s?(#\w+)')
-themeColor = []
+skinReg = re.compile(r'\$slave:\s?(#\w+)')
+skinFileReg = re.compile(r'skin\d+')
 
-for path in IO_util.get_all_file(filePath):
-    if path.find('theme') != -1:
-        with open(path, 'r', encoding='utf-8') as file:
-            for line in file.readlines():
-                result = theme4Reg.search(line)
-                if result:
-                    themeColor.append(result.groups()[0])
-                    break
 
-print(','.join(themeColor))
+def get_theme_old():
+    # 获取theme4
+    file_path = r'E:\SHT\project\sas-web\src\main\webapp\src\default\scss'
+    theme_color = []
+
+    for path in IO_util.get_all_file(file_path):
+        if path.find('theme') != -1:
+            with open(path, 'r', encoding='utf-8') as file:
+                for line in file.readlines():
+                    result = theme4Reg.search(line)
+                    if result:
+                        theme_color.append(result.groups()[0])
+                        break
+    return theme_color
+
+
+def get_theme_new():
+
+    file_path = r'E:\SHT\project\sas-web\src\main\webapp\src\saishi\scss'
+    theme_color = []
+
+    for path in IO_util.get_all_file(file_path):
+        if skinFileReg.search(path):
+            with open(path, 'r', encoding='utf-8') as file:
+                for line in file.readlines():
+                    result = skinReg.search(line)
+                    if result:
+                        theme_color.append(result.groups()[0])
+                        break
+    return theme_color
+
+
+defaultColor = '#29d8c3'
+print(defaultColor + ',' + ','.join(get_theme_new()))
+
--- py/test/base64ToImg.py
+++ py/test/base64ToImg.py
@@ -2,15 +2,13 @@ import os
 from py.utils import IO_util
 
 writePath = r'E:\git\pythonCode\src\theme'
+writePath2 = r'E:\SHT\project\sas-web\src\main\webapp\res\saishi'
 base64FilePath = r'E:\git\pythonCode\src\base64.txt'
 
 with open(base64FilePath, 'r', encoding='utf-8') as file:
     content = file.read()
     base64Arr = content.split('###')
     for i, base64str in enumerate(base64Arr):
-        # l = len(base64str)
-        # if l % 3 != 0:
-        #     base64str += (3 - l % 3) * '='
-        dir_path = os.path.join(writePath, str(i + 1))
+        dir_path = os.path.join(writePath2, str(i + 1))
         IO_util.base64_to_img(base64str[22:], os.path.join(dir_path, 'countdown_arc_bg.png'))
 
