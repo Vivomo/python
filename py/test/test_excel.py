731f4e277b6ea159efa3f777fbffe8db2e0ecc18
vivomo <929992114@qq.com>
2016-11-09 19:41:32 +0800

    no message

--- py/test/test_excel.py
+++ py/test/test_excel.py
@@ -1,10 +1,17 @@
 import xlrd
+import xlwt
+from xlutils.copy import copy
+import openpyxl
 
 fileName = r'E:\git\pythonCode\src\test.xls'
+fileName2 = r'E:\git\pythonCode\src\ignore\data.xlsx'
+bk = xlrd.open_workbook(fileName2)
+remarks = []
+lines = []
+remark = ''
 
-bk = xlrd.open_workbook(fileName)
 try:
-    sh = bk.sheet_by_name("参赛者信息")
+    sh = bk.sheet_by_name("家庭亲子跑选手信息")
     # 获取行数
     nrows = sh.nrows
     # 获取列数
@@ -12,15 +19,42 @@ try:
     print("nrows %d, ncols %d" % (nrows, ncols))
 
     # 获取各行数据
-    
+
     for i in range(1, nrows):
         row_data = sh.row_values(i)
-        cell_value = sh.cell_value(i, 1)
-        xlrd.xldate_as_tuple(cell_value, 0)
-        print(xlrd.xldate_as_tuple(cell_value, 0))
-        # print(cell_value)
-        print(row_data)
+        number = row_data[0]
+        name = row_data[1]
+        if number:
+            remarks.append(remark)
+            lines.append(i)
+            remark = name
+        else:
+            remark += name
+
+        # cell_value = sh.cell_value(i, 1)
+        # xlrd.xldate_as_tuple(cell_value, 0)
+        # print(xlrd.xldate_as_tuple(cell_value, 0))
+        # # print(cell_value)
+        # print(row_data)
+    remarks = remarks[1:]
+    print(remarks)
 
 except Exception as e:
     print("no sheet in %s named 参赛者信息" % fileName)
 
+
+try:
+    wk = openpyxl.load_workbook(fileName2)
+
+    ws = wk.get_sheet_by_name('家庭亲子跑选手信息')
+
+    for index, name in enumerate(remarks):
+        point = 'H%d' % (lines[index] + 1)
+        print(point, name)
+        ws[point] = name
+        cell = ws[point]
+        print(cell)
+
+    wk.save(r'E:\git\pythonCode\src\ignore\data2.xlsx')
+except Exception as e:
+    print(e)
