234c1482b1b2886d2f86483d89996c5a6a7b8b5a
vivomo <929992114@qq.com>
2016-11-24 18:44:59 +0800

    no message

--- py/autoWeb/task.py
+++ py/autoWeb/task.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import Select, WebDriverWait
 import time
 import os
 
@@ -13,23 +13,35 @@ class Task(object):
     def __init__(self, options):
         self.driver = webdriver.Chrome(driver_path)
         self.domain = options['domain']
+        self.wait = WebDriverWait(self.driver, 10)
 
-    def event_signup(self):
-        pass
+    @staticmethod
+    def dom_ready(driver):
+        state = driver.execute_script('return document.readyState')
+        return state == 'complete'
+
+    def event_signup(self, event_id):
+        driver = self.driver
+        driver.get('%s/event?id=%s' % (self.domain, event_id))
+        self.wait.until(self.dom_ready)
+        apply_elem = driver.find_element_by_class_name('join')
+        apply_elem.click()
 
     def login(self, username, password):
         driver = self.driver
         login_url = self.domain + '/login'
         driver.get(login_url)
-        time.sleep(2)
+        # time.sleep(2)
         form = driver.find_element_by_class_name('loginform')
         e_username = form.find_element_by_name('username')
         e_password = form.find_element_by_name('password')
         e_username.send_keys(username)
         e_password.send_keys(password)
         form.submit()
+        time.sleep(2)
 
 task = Task({
-    'domain': 'http://vimo.360jlb.cn'
+    'domain': 'http://vm.360jlb.cn'
 })
 task.login('929992114@qq.com', '000000')
+task.event_signup(35712)
