13c9c1622239ba84e1c1f54510c7bc4165181483
vivomo <929992114@qq.com>
2017-02-05 19:55:22 +0800

    no message

--- py/test/one_line_love.py
+++ py/test/one_line_love.py
@@ -1,4 +1,4 @@
-print('\n'.join([''.join([('LoveSusu'[(x - y) % 8]
+print('\n'.join([''.join([('********'[(x - y) % 8]
                            if ((x * 0.05) ** 2 + (y * 0.1) ** 2 - 1) ** 3 - (x * 0.05) ** 2 * (y * 0.1) ** 3 <= 0
                            else' ')
                           for x in range(-30, 30)]) for y in range(15, -15, -1)]))
