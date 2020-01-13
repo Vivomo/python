c27af347ced67c67b10e85119c21cc743010ddf3
Vimo <929992114@qq.com>
2020-01-14 03:34:20 +0800

    测试python声音控制

---
+++ py/sound/beep.py
@@ -0,0 +1,7 @@
+import winsound
+duration = 5000  # millisecond
+freq = 37  # Hz
+winsound.Beep(freq, duration)
+# winsound.PlaySound('ALARM8', winsound.SND_ASYNC)
+# winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
+# winsound.PlaySound("*", winsound.SND_ALIAS)
